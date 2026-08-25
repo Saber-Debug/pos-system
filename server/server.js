9b0a8a9b77e06d9d74d6f73c2544c42d8ad4a90a
Gilgamesh <itzpekungzgamerx@gmail.com>
2026-08-25 09:53:13 +0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK", message: "Server is running!" });
+});
+
+app.listen(3000, () => {
+  console.log("Backend running on http://localhost:3000");
+});
